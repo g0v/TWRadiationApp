11accb29c426cbd0209c507262550adef789edf6
rokaku <rokaku@users.noreplay.github.com>
2014-06-12 10:06:25 +0800

    Add icons and images
    Add icons and images

---
+++ TWRadiationApp/View/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  TWRadiationApp
+//
+//  Created by Charles Lu on 14/6/11.
+//  Copyright (c) 2014年 g0v.tw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ TWRadiationApp/View/LoginViewController.m
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.m
+//  TWRadiationApp
+//
+//  Created by Charles Lu on 14/6/11.
+//  Copyright (c) 2014年 g0v.tw. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+@property (nonatomic) IBOutlet UIBarButtonItem* revealButtonItem;
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)login:(id)sender {
+    [self.navigationController popToRootViewControllerAnimated:YES];
+}
+@end
