0b64a54d31d29e4e6454bd85bec1fcfee259313d
rokaku <rokaku@users.noreplay.github.com>
2014-06-12 10:06:25 +0800

    Add icons and images
    Add icons and images

--- TWRadiationApp/View/SidebarViewController.m
+++ TWRadiationApp/View/SidebarViewController.m
@@ -76,6 +76,38 @@
     return cell;
 }
 
+
+- (void) prepareForSegue: (UIStoryboardSegue *) segue sender: (id) sender
+{
+    // configure the destination view controller:
+/*    if ( [segue.destinationViewController isKindOfClass: [ColorViewController class]] &&
+        [sender isKindOfClass:[UITableViewCell class]] )
+    {
+        UILabel* c = [(SWUITableViewCell *)sender label];
+        ColorViewController* cvc = segue.destinationViewController;
+        
+        cvc.color = c.textColor;
+        cvc.text = c.text;
+    }*/
+    
+    // configure the segue.
+    if ( [segue isKindOfClass: [SWRevealViewControllerSegue class]] )
+    {
+        SWRevealViewControllerSegue* rvcs = (SWRevealViewControllerSegue*) segue;
+        
+        SWRevealViewController* rvc = self.revealViewController;
+        NSAssert( rvc != nil, @"oops! must have a revealViewController" );
+        
+        NSAssert( [rvc.frontViewController isKindOfClass: [UINavigationController class]], @"oops!  for this segue we want a permanent navigation controller in the front!" );
+        
+        rvcs.performBlock = ^(SWRevealViewControllerSegue* rvc_segue, UIViewController* svc, UIViewController* dvc)
+        {
+            UINavigationController* nc = [[UINavigationController alloc] initWithRootViewController:dvc];
+            [rvc pushFrontViewController:nc animated:YES];
+        };
+    }
+}
+
 /*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
