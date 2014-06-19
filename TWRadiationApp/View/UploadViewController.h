f84705e0cfa2914658e7c97e4f5b61336ef677b7
rokaku <rokaku@users.noreplay.github.com>
2014-06-19 10:05:36 +0800

    Add upload UI and fix incorrect login UI flows

---
+++ TWRadiationApp/View/UploadViewController.h
@@ -0,0 +1,13 @@
+//
+//  UploadViewController.h
+//  TWRadiationApp
+//
+//  Created by Charles Lu on 14/6/18.
+//  Copyright (c) 2014年 g0v.tw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UploadViewController : UIViewController
+
+@end
